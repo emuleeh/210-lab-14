6482d47ba37ffb34b605aed6251815542b2ad47d
Emily <emuleehw@gmail.com>
2024-09-29 21:00:33 -0700

    Second commit

--- 210-lab-14-/main.cpp
+++ 210-lab-14-/main.cpp
@@ -7,4 +7,14 @@ const int W12 = 12;
 
 class Color {
 private:
+    string name;
+    int red;
+    int green;
+    int blue;
+public:
+    //getters and setters
+    string getName()        { return name; }
+    void setName(string n)  { name = n; }
+    int getRed()            { return red; }
+    void setRed(int r)      { red = r; }
 }
